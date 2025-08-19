3201be7075bbffdd732eb34348c4f43483f10ecc
Suryadeep Singh <180669860+sudosuryadeep@users.noreply.github.com>
2025-08-19 17:46:30 +0530

    Update App.jsx with add /Auth folder dir for Login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 // Pages
 import Home from "./pages/Home";
-import Login from "./pages/Login";
+import Login from "./pages/Auth/Login";
 import Register from "./pages/Register"; // Register Page
 import ForgotPassword from "./pages/ForgotPassword"; // Forgot Password Page
 import AddCourse from "./pages/AddCourses";
